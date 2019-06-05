980b19ae25f2b97737af05f11030295efb144798
lucasmcar <lmc.lucas90@gmail.com>
2019-06-05 20:38:36 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
+        
+        <link rel="stylesheet" href="node_modules/bootstrap/compiler/estilo.css">
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            
+        </nav>
+        <header class="sics-hdr">
+            <h2>SICS -Sistema  Integrado de Consulta de Serviços</h2>
+            <small class="sml-header">O sistema pode ser integrado com qualquer oficina</small>
+        </header>
+        <section>
+        <form class="sics-form">
+            <input type="text" placeholder="XX-000000" class="sics-input-text sics-button-primary">
+            <button type="submit" name="btSubmit" class="sics-button sics-button-secondary">Consultar</button>
+            <small>O código deve ser no formato</small>
+        </form>
+        </section>   
+       
+    </body>
+</html>
---
+++ minhaconsulta.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
+        
+        <link rel="stylesheet" href="node_modules/bootstrap/compiler/estilo.css">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Logout</li>
+            </ul>
+        </nav>
+        <div class="container">
+        <div class="sics-details-wrapper"><!--div principal-->
+            <div class="hdrs">
+                <h3>Detalhes do serviço</h3>
+                <h6>[DADOS DA OFICINA]</h6>
+            </div>
+        <div class="sics-left-details">
+            <ul class="sics-list">
+                <li class="sics-list-item"><strong>Código: </strong> RT-999999</li>
+                 <li class="sics-list-item"><strong>Veículo: </strong> 206</li>
+                 <li class="sics-list-item"><strong>Marca: </strong>Peugeot</li>
+                 <li class="sics-list-item"><strong>Código: </strong>RT-999999</li>
+            </ul>
+            
+        </div>
+        <div class="sics-right-details">
+            <ul class="sics-list">
+                <li class="sics-list-item "><strong>Descrição: </strong> Paralama e porta</li>
+                <li class="sics-list-item "><strong>Etapa: </strong>Iniciando</li>
+                <li class="sics-list-item"><strong>Conclusão (%):</strong>15.0</li>
+                <button class="sics-button sics-button-primary align-right">Ver mais</button>
+            </ul>
+        </div>
+        
+            
+        
+        </div><!--/div principal-->
+        <div id="escondida"></div>
+        </div>
+    </body>
+</html>
